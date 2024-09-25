835b41618154c205d31118f8a1cc0f7bec82a881
ArjanJhamesM <arjanjhamesm@gmail.com>
2024-09-25 22:35:01 +0800

    Save functioning UI

--- Assets/Scripts/Variables.cs
+++ Assets/Scripts/Variables.cs
@@ -2,87 +2,79 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using TMPro;
 
 public class Variables : MonoBehaviour
 {
+    [SerializeField] private TextMeshProUGUI cloudIndicator;
+    [SerializeField] private TextMeshProUGUI rainIndicator;
+    [SerializeField] private TextMeshProUGUI snowIndicator;
+    [SerializeField] private TextMeshProUGUI skyIndicator;
+    [SerializeField] private TextMeshProUGUI starsIndicator;
+    [SerializeField] private TextMeshProUGUI sunAndMoonIndicator;
+    [SerializeField] private TextMeshProUGUI fogIndicator;
+
+
     bool showClouds = true;
     bool showRain = true;
     bool showSnow = true;
     bool showSky = true;
     bool showStars = true;
-    bool showSun_and_Moon = true;
+    bool showSunAndMoon = true;
     bool showFog = true;
 
-    private void Statements()
 
+    public void ChangeValue(ref bool showSetting, ref TextMeshProUGUI settingIndicator)
     {
-        if (showClouds == true)
-        {
-            showClouds = false;
-        }
-        else
-        {
-            showClouds = true;
-        }
-       
-        if (showRain == true)
-        {
-            showRain = false;
-        }
-        else
-        {
-            showRain = true;
-        }
-       
-        if (showSnow == true)
-        {
-            showSnow = false;
-        }
-        else
-        {
-            showSnow = true;
-        }
-       
-        if (showSky == true)
-        {
-            showSky = false;
-        }
-        else
-        {
-            showSky = true;
-        }
-        
-        if (showStars == true)
-        {
-            showStars = false;
-        }
-        else
-        {
-            showStars = true;
-        }
-       
-        if (showSun_and_Moon == true)
-        {
-            showSun_and_Moon = false;
-        }
-        else
-        {
-            showSun_and_Moon = true;
-        }
-        
-        if (showFog == true)
-        {
-            showFog = false;
-        }
-        else
-        {
-            showFog = true;
-        }
 
+        if (showSetting == true)
+        {
+            showSetting = false;
+            settingIndicator.text = "OFF";
+            print(showSetting);
+        }
+        else if (showSetting == false)
+        {
+            showSetting = true;
+            settingIndicator.text = "ON";
+            print(showSetting);
+        }
     }
 
+    public void ShowClouds()
+    {
+        ChangeValue(ref showClouds, ref cloudIndicator);
+    }
 
+    public void ShowRain()
+    {
+        ChangeValue(ref showRain, ref rainIndicator);
+    }
 
+    public void ShowSnow()
+    {
+        ChangeValue(ref showSnow, ref snowIndicator);
+    }
+
+    public void ShowSky()
+    {
+        ChangeValue(ref showSky, ref skyIndicator);
+    }
+
+    public void ShowStars()
+    {
+        ChangeValue(ref showStars, ref starsIndicator);
+    }
+
+    public void ShowSunAndMoon()
+    {
+        ChangeValue(ref showSunAndMoon, ref sunAndMoonIndicator);
+    }
+
+    public void ShowFog()
+    {
+        ChangeValue(ref showFog, ref fogIndicator);
+    }
 }
    
 
